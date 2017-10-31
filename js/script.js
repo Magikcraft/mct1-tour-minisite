903d41809577834a363cc43f1de8c7c90a7c4e2e
Jonn Dillon <jonn_dillon@iinet.net.au>
2017-10-31 17:02:09 +1000

    Rebuilt CSS to LESS, refined JS and HTML, laid out structure. CSS is broken atm

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-			menu: '#menu',
-			anchors: ['firstPage', 'secondPage', '3rdPage', 'fourthPage'],
+			menu: '#navbar',
+			anchors: ['homePage', 'secondPage', 'thirdPage', 'fourthPage'],
 			sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', 'red'],
-			autoScrolling: false,
+            autoScrolling: false,
+            keyboardScrolling: true,
 	});
 });
