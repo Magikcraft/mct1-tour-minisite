a51bb0a58e9af6b5f356b1deba1fe75984e9fa9a
Jonn Dillon <jonn_dillon@iinet.net.au>
2017-11-02 01:24:35 +1000

    Finished responsive nav, fixed most issues. Somehow broke the scrolling.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-			menu: '#navbar',
-			anchors: ['homePage', 'secondPage', 'thirdPage', 'fourthPage'],
-			sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', 'red'],
+			menu:            '#navbar',
+			anchors:        ['0page', '1page', '2page', '3page', '4page', '5page', '6page', '7page', '8page', '9page', '10page'],
+			sectionsColor:  ['black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 'black' ],
             autoScrolling: false,
             keyboardScrolling: true,
 	});
